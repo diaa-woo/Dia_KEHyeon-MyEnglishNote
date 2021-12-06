a56de1c2ead8c2387e0f563491c1228aa54ccd07
diadntjr <dntjr0501@gmail.com>
2021-12-06 17:03:30 +0900

    초기 커밋.

---
+++ (Dia,KEHyeon)-MyEnglishNote.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct listNode {
+    struct listNode* link;
+}listNode;
+
+int InputEnglishWord() {
+
+}
+
+int main()
+{
+
+}
