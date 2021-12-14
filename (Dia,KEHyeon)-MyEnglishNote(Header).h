55c32d1dbe9dd253892eb7f2eb7ce78ba01e0bcc
diadntjr <dntjr0501@gmail.com>
2021-12-15 06:08:38 +0900

    Update Linux version Program and Header File

---
+++ (Dia,KEHyeon)-MyEnglishNote(Header).h
@@ -0,0 +1,52 @@
+#define MAX_LENGTHS 25
+
+typedef struct listNode {
+    char data[25];
+    struct listNode* link;
+}listNode;
+
+typedef struct {
+    listNode* head;
+}linkedList_h;;
+
+// 공백 연결 리스트 생성
+linkedList_h* createLinkedList_h();
+
+// 연결 리스트의 전체 메모리를 해제하는 연산
+void freeLinkedList_h(linkedList_h* L);
+
+// 연결 리스트를 순서대로 출력하는 연사
+void printList(linkedList_h* L);
+
+// 첫번째 노드로 삽입하는 연산
+void insertFirstNode(linkedList_h* L, char* x);
+
+// 노드를 pre 뒤에 삽입하는 연산
+void insertMiddleNode(linkedList_h* L, listNode* pre, char* x);
+
+// 마지막에 노드를 삽입하는 연산
+void insertLastNode(linkedList_h* L, char* x);
+
+// 리스트에서 x 노드를 탐색하기
+listNode* searchNode(linkedList_h* L, char* x);
+
+//프로그램 사용 메뉴얼을 알려줌
+void manual();
+
+// 프로그램 맨 처음 실행시 여러 문장들을 출력함
+void printFirst();
+
+// 메뉴들을 출력해주고 메뉴값을 리턴시켜줌
+void printMenu();
+
+//새로운 영어단어를 입력받아줌
+void NewEnglishVoca();
+
+//영어단어를 삭제시켜줌
+void DeleteEnglishVoca();
+
+//현재 연결리스트에 존재하는 모든 영어단어들을 출력해줌
+void PrintEnglishVoca();
+
+//현재 연결리스트에 존재하는 영단어들을 가지고 테스트를 진행함
+void TestEnglishVoca();
---
+++ (Dia,KEHyeon)-MyEnglishNote(for_Linux).c
@@ -0,0 +1,151 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <(Dia,KEHyeon)-MyEnglishNote(Header).h>
+
+int main() {
+    int menu = 1;
+    linkedList_h* L;
+    listNode* p;
+
+    printf("Making LinkedList...");
+    L = createLinkedList_h();  //create new linkedlist
+
+    printFirst();  //Print some sentences when it start
+
+    while(menu != 0) {
+        menu = printMenu;  //Print menu and return to mode number
+        if(menu == 0) break;
+        else switch(menu) {
+            case 1:
+                manual();
+                break;
+            case 2:
+                NewEnglishVoca();
+                break;
+            case 3:
+                DeleteEnglishVoca();
+                break;
+            case 4: 
+                PrintEnglishVoca();
+                break;
+            case 5:
+                TestEnglishVoca();
+        }
+    }
+    
+    printf("Unlink LinkedList...");
+    freeLinkedList_h(L);
+    return 0;
+}
+
+void manual() {
+
+}
+
+void printFirst() {
+    printf("===============\n");
+    printf("This is your own computer English vocabulary!\n");
+} 
+
+void printMenu() {
+    int menu;
+    printf("===============\n");
+    printf("Here is your menu!\n");
+    printf("1. Manual!");
+    printf("2. Enter New English vocabulary.\n");
+    printf("3. Delete English vocabulary.\n");
+    printf("4. Print all vocabulary.\n");
+    printf("5. Quiz\n");
+    printf("0: End this program.");
+    printf("===============\n");
+    printf("What do you want to do? : ");
+    scanf("%d",&menu);
+    while(1) {
+        if(menu < 6) return menu;
+        printf("You entered wrong number! Please enter again! : ");
+        scanf("%d",&menu);
+    }
+    
+}
+
+void NewEnglishVoca() {
+
+}
+
+void DeleteEnglishVoca() {
+
+}
+
+void PrintEnglishVoca() {
+
+}
+
+void TestEnglishVoca() {
+
+}
+
+linkedList_h* createLinkedList_h() {
+    linkedList_h* L;
+    L = (linkedList_h*)malloc(sizeof(linkedList_h));
+    L->head = NULL;
+    return L;
+}
+
+void freeLinkedList_h(linkedList_h* L) {
+    free(L);
+}
+
+void printList(linkedList_h* L) {
+    listNode* p;
+    printf("L = (");
+    p = L->head;
+    while (p!=NULL) 
+    {
+        printf("%s", p->data);
+        p = p->link;
+        if (p != NULL) printf(",");
+    }
+    printf(") \n");
+}
+
+void insertFirstNode(linkedList_h* L, char* x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode->data, x);
+    newNode->link = L->head;
+    L->head = newNode;
+}
+
+void insertMiddleNode(linkedList_h* L, listNode* pre, char* x) {
+    listNode* newNode;
+    newNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(newNode->data, x);
+    newNode->link = pre->link;
+    pre->link = newNode;
+}
+
+void insertLastNode(linkedList_h* L, char* x) {
+    listNode* lastNode;
+    listNode* temp;
+    lastNode = (listNode*)malloc(sizeof(listNode));
+    strcpy(lastNode->data, x);
+    lastNode->link = NULL;
+    if (L->head == NULL) {
+        L->head = lastNode;
+        return;
+    }
+
+    temp = L->head;
+    while (temp->link != NULL) temp = temp->link;
+    temp->link = lastNode;
+}
+
+listNode* searchNode(linkedList_h* L, char* x) {
+    listNode* p;
+    p = L->head;
+    while (p != NULL) {
+        if (strcmp((p->data), x) == 0) return p;
+        p = p->link;
+    }
+}
